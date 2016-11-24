d8b4e62354b560f25468c4ae93123954c601a16b
albertocomu <albertocomu@gmail.com>
2016-11-24 18:56:14 +0100

    Initial Commit

---
+++ tresEnRayas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tresEnRayas
+//
+//  Created by Alberto Comuñas Lopez on 24/11/16.
+//  Copyright © 2016 Alberto Comuñas Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
